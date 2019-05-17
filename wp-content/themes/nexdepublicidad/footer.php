a76d624c8affd690f51eafb31f910282d5e7ba55
ja10ns0 <alonsofernandezjaime@gmail.com>
2019-05-17 22:00:59 +0200

    Wordpress 5.2 update Nex theme: -Footer and main implementation

--- wp-content/themes/nexdepublicidad/footer.php
+++ wp-content/themes/nexdepublicidad/footer.php
@@ -1,3 +1,6 @@
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">footer</div>
+</footer>
 <?php wp_footer(); ?>
     </body>
 </html>
--- wp-content/themes/nexdepublicidad/header.php
+++ wp-content/themes/nexdepublicidad/header.php
@@ -1,40 +1,48 @@
 <!DOCTYPE html>
-<html <?php language_attributes()?>>
+<html <?php language_attributes()?> class="h-100">
 <head>
     <meta <?php bloginfo('charset')?>>
     <title><?php wp_title(''); echo ' | ';  bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<body class="d-flex flex-column h-100">
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">nex</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">el croquis</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">nextoria</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">contactanex</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">es</a>
-      </li>      
-      <li class="nav-item">
-        <a class="nav-link" href="#">en</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">nex</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">el croquis</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">nextoria</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">contactanex</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">es</a>
+            </li>      
+            <li class="nav-item">
+              <a class="nav-link" href="#">en</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main class="flex-shrink-0">
+    <div class="container">container</div>
+  </main>
